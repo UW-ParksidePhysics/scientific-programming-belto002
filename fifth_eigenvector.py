ece7c890a8ef15bcec27ea7f05dd98d8dfd9e220
belto002 <belto002@rangers.uwp.edu>
2024-05-08 16:30:03 -0500

    fifth_eigenvector.py

--- fifth_eigenvector.py
+++ fifth_eigenvector.py
@@ -1,42 +1,42 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-elements = [[2, -1, 0, 0], [-1, 2, -1, 0], [0, -1, 2, -1], [0,0, -1, 2]]
-diag_elements np.diagflat(elements)
+elements = [[2, -1, 0, 0], [-1, 2, -1, 0], [0, -1, 2, -1], [0, 0, -1, 2]]
+diag_elements = np.diagflat(elements)
 
-#define the scalar
-scalar = 1 / (2 * ((1 / (5+1) ** 2)))
+# define the scalar
+scalar = 1 / (2 * (1 / (5 + 1) ** 2))
 
-#multiply diagonal matrix by the scalar
+# multiply diagonal matrix by the scalar
 H = scalar * diag_elements
 
 print(H)
 
-#Define a matrix H
+# Define a matrix H
 H = np.array([[2, -1, 0, 0], [-1, 2, -1, 0], [0, -1, 2, -1], [0, 0, -1, 2]])
 
 # Compute the eigenvectors and eigenvalues of H
 eigenvalues, eigenvectors = np.linalg.eig(H)
 
-#sort eigenvaule and eigenvectors
+# sort eigenvalue and eigenvectors
 x = eigenvalues.argsort()
 eigenvalues = eigenvalues[x]
 eigenvectors = eigenvectors[:, x]
 
-#define x Values
-x_values = np.linsspace(1/(5+1), 5/(5+1), 5)
+# define x Values
+x_values = np.linspace(1 / (5 + 1), 5 / (5 + 1), 5)
 
-#plot fifth eigenvector
+# plot fifth eigenvector
 plt.plot(eigenvectors[:, 4], label="Fifth Eigenvector")
 
-#labels
+# labels
 plt.xlabel('x')
 plt.ylabel('value')
 plt.legend()
 
-#plot the sqrt 2 sin pix functuin
+# plot the sqrt 2 sin pix function
 sin = np.sqrt(2) * np.sin(np.pi * x_values)
 plt.plot(x_values, sin)
 
-#show plot
+# show plot
 plt.show()
